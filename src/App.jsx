509b86323dce2077f9384b8123ae9b96afcc148a
Gary Lee <dokdo2005@gmail.com>
2022-10-06 20:07:29 +0900

    feat: 기능 마이그레이션 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import images from "./data/images.json";
+import "./css/App.css";
+import download from "downloadjs";
+import * as htmlToImage from "html-to-image";
+import { Button, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudArrowDown, faEraser } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentImg, setCurrentImg] = useState(0);
+  const [imageText, setImageText] = useState(images[currentImg].text);
+  const textInputRef = useRef(null);
+
+  useEffect(() => {
+    resetImageText();
+  }, [currentImg]);
+
+  const handleChangeImage = (value) => {
+    setCurrentImg(value);
+  };
+
+  const resetImageText = () => {
+    setImageText(images[currentImg].text);
+    textInputRef.current.value = images[currentImg].text;
+  };
+
+  const handleDownloadImage = () => {
+    htmlToImage
+      .toPng(document.getElementById("imageArea"))
+      .then((dataUrl) => {
+        download(dataUrl, `loa-meme-${Date.now()}.png`);
+      })
+      .catch(function (error) {
+        console.error("다운로드 중 오류 발생", error);
+      });
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="appTitle">로스트아크 짤 생성기</div>
+      <div>&nbsp;</div>
+      <div id="imageArea" className="imageArea">
+        <img src={images[currentImg].src} width="720px" height="405px" />
+        <div className="imageText">{imageText}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>&nbsp;</div>
+      <div className="optionArea">
+        <Form.Select
+          defaultValue={currentImg}
+          onChange={(e) => handleChangeImage(e.target.value)}
+        >
+          {images.map((el, idx) => (
+            <option key={idx} value={el.id}>
+              {el.name}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Control
+          type="text"
+          ref={textInputRef}
+          defaultValue={imageText}
+          onChange={(e) => setImageText(e.target.value)}
+        />
+        <Button variant="primary" onClick={() => handleDownloadImage()}>
+          <FontAwesomeIcon icon={faCloudArrowDown} />
+        </Button>
+        <Button variant="danger" onClick={() => resetImageText()}>
+          <FontAwesomeIcon icon={faEraser} />
+        </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="appFooter">Made by 김뷰엘 with 💖</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
